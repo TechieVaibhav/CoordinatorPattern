17ecf0a3089e30930b844a843de32ac0b2886428
TechieVaibhav <vaibhav.sharma.jp@gmail.com>
2022-02-27 20:39:49 +0530

    Add Second View Controller

---
+++ CoordinatorPattern/CoordinatorPattern/SecondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SecondViewController.swift
+//  CoordinatorPattern
+//
+//  Created by Guest User 2 on 27/02/22.
+//  Copyright © 2022 Guest User 2. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
