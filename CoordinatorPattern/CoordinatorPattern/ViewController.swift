97d87227497987d25d2845111fc1d02529a3d8ec
TechieVaibhav <vaibhav.sharma.jp@gmail.com>
2022-02-27 20:33:04 +0530

    initial setup

---
+++ CoordinatorPattern/CoordinatorPattern/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoordinatorPattern
+//
+//  Created by Guest User 2 on 27/02/22.
+//  Copyright © 2022 Guest User 2. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
